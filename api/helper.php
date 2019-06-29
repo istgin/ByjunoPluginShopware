ddef94b9bd59c0f05b2a3b93531c476bff293f46
Igor <igor.sutugin@gmail.com>
2019-06-29 14:46:29 +0300

    1.0.6

--- api/helper.php
+++ api/helper.php
@@ -274,7 +274,7 @@ function CreateShopWareShopRequestUserBilling($user, $billing, $shipping, $contr
     }
 
     $extraInfo["Name"] = 'CONNECTIVTY_MODULE';
-    $extraInfo["Value"] = 'Byjuno ShopWare module 1.0.5';
+    $extraInfo["Value"] = 'Byjuno ShopWare module 1.0.6';
     $request->setExtraInfo($extraInfo);
     return $request;
 
